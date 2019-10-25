339b32f127cc12bcb10652aa539c133ac62c3a3a
YUTA <33199257+YutaDev@users.noreply.github.com>
2019-10-25 16:40:00 +0900

    メインロジック

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MultiTaskServerInspection.UI;
+using System.Threading;
+
+namespace MultiTaskServerInspection
+{
+    static class EntryPoint
+    {
+        public  static MainForm mf = null;
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string MutexName = "ServerInspectionTool";
+            Mutex  mutex = new Mutex(false, MutexName);
+
+            bool hasHandle = false;
+
+            try
+            {
+                try
+                {
+                    hasHandle = mutex.WaitOne(0, false);
+                }
+                catch (AbandonedMutexException)
+                {
+                    hasHandle = true;
+                }
+
+                if (hasHandle == false)
+                {
+                    MessageBox.Show("多重起動できません",
+                            "エラー",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error
+                            );
+                    return;
+                }
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            finally
+            {
+                if (hasHandle)
+                {
+                    mutex.ReleaseMutex();
+                }
+                mutex.Close();
+            }
+        }
+    }
+}
